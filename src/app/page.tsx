b91bae25f5586482e08aef19ca63d7d0ce1ad656
Noah Seltzer <noahseltzer@outlook.com>
2024-06-12 16:56:33 -0700

    trigger build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/hero.tsx";
 import Stack from "@/components/stack.tsx";
+import { Projects } from "@/components/projects";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="flex flex-col w-full">
         <Hero />
         <Stack />
+        <Projects />
       </div>
     </main>
   );
---
+++ src/components/projects.tsx
@@ -0,0 +1,18 @@
+import { Divider, Typography } from '@mui/material'
+
+export const Projects = () => {
+    return (
+        <div className='flex flex-col items-center'>
+            <div className='w-5/6 my-4'>
+                <Typography variant='h2'>Projects</Typography>
+            </div>
+            <Divider />
+            <div className='w-5/6'>
+                <Typography variant='h6'>
+                    React SVG Editor
+                </Typography>
+                <Typography></Typography>
+            </div>
+        </div>
+    )
+}
